b325e16dbc202f85abfa652ce4aff2f1790eac42
Savio Nascimento <savionascimentodev@gmail.com>
2023-01-30 23:49:34 -0400

    ✨ added section 1 - primitive types

---
+++ Js-Tipos-Variaveis-Funcoes/readme.md
@@ -0,0 +1,5 @@
+### Váriaveis
+
+Quando um programa precisa reter um valor para uso futuro, ele **atribui** o valor a (ou **“armazena”** o dado em) uma **variável**. As variáveis têm nomes e permitem o uso desses nomes em nossos programas para se referir a valores. A maneira como as variáveis funcionam é outra característica fundamental de qualquer linguagem de programação.
+
+Os **tipos de JavaScript** podem ser divididos em duas categorias: tipos **primitivos** e tipos de **objetos**: Os tipos **primitivos** do JavaScript incluem **números**, palavras ou texto (conhecidas como **strings**) e valores **booleanos** (conhecidos como booleanos).
---
+++ Js-Tipos-Variaveis-Funcoes/types.js
@@ -0,0 +1,44 @@
+/* ----------------------------------------------------------------- */
+// Type Number 
+
+const primeiroNumero = 10
+const segundoNumero = 4
+
+// Com váriaveis do tipo Number podemos fazer operações númericas;
+
+const soma = primeiroNumero + segundoNumero // 14
+const divisao = primeiroNumero / segundoNumero // 2.5
+const subtracao = primeiroNumero - segundoNumero // 6
+const multiplicacao = primeiroNumero * segundoNumero // 40
+
+// Também podemos usar pontos flutuantes;
+
+const numero1 = 5.5 // Lembrando usa-se o (ponto), não a virgula;
+const numero2 = .5
+
+// NaN -> Not a Number (não é um número);
+
+const nome = 'Savio'
+console.log(nome * primeiroNumero) // NaN
+
+/* ----------------------------------------------------------------- */
+// Type String
+
+// Tipos de váriaveis no tipo texto, podemos usar tanto aspas simples ', como duplas ";
+const texto1 = 'Meu nome é ';
+
+// Concatenação (+);
+console.log(texto1 + nome)
+
+// Template String OU template literal;
+
+/* ----------------------------------------------------------------- */
+// Type Boolean
+
+const boolean1 = 12
+const boolean2 = 14
+
+console.log(boolean1 === boolean2) // false;
+
+// true -> verdadeiro;
+// false -> falso;
